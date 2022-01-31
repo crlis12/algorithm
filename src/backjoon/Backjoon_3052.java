97d865f23c33d946660433e59897dd940c670619
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-01-31 19:58:44 +0900

    백준 3052번 추가

---
+++ src/backjoon/Backjoon_3052.java
@@ -0,0 +1,50 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Backjoon_3052 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		
+//		int[] arr = new int[10];
+//		int[] arrRemainder = new int[10]; 
+//		int realCount =0;		
+//		
+//		for(int i =0; i < 10; i++) {
+//			arr[i] = Integer.parseInt(br.readLine());
+//			arrRemainder[i] = arr[i] % 42 ;
+//		}
+//		
+//		for(int i =0; i< arrRemainder.length; i++) {
+//			int count = 0;
+//			for(int j =i+1; j < arrRemainder.length; j++) {
+//				if(j > arrRemainder.length) {
+//					break;
+//				}
+//				if(arrRemainder[i] == arrRemainder[j]) {
+//					count += 1;
+//				}
+//			}
+//			if(count == 0) {
+//				realCount += 1;
+//			}
+//		}
+//		System.out.println(realCount);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> h = new HashSet<Integer>();  //중복된 값 제거
+		
+		for(int i =0; i<10; i++) {
+			h.add(Integer.parseInt(br.readLine())%42 );
+		}
+		
+		System.out.println(h.size());
+	}
+
+}
