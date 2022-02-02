b67f531da3bab8b8b70e861cdd96eb07d257f9a1
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-02-02 16:16:41 +0900

    4673

---
+++ src/backjoon/Backjoon_4673.java
@@ -0,0 +1,37 @@
+package backjoon;
+
+public class Backjoon_4673 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean[] check = new boolean[10001];
+
+		for (int i = 1; i < 10001; i++) {
+			int n = d(i);
+
+			if (n < 10001) {
+				check[n] = true;
+			}
+		}
+
+		for (int i = 1; i < 10001; i++) {
+			if(check[i] == false) {		
+				System.out.println(i);
+			}
+		}
+
+	}
+
+	public static int d(int num) {
+		int sum = num;
+
+		while (num != 0) {
+			sum = sum + (num % 10);
+			num = num / 10;
+		}
+
+		return sum;
+	}
+
+}
