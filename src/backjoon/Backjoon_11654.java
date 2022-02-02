3916116346a0cf0bf82690ed2705e850b68c0b3f
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-02-02 17:41:44 +0900

    11654번

---
+++ src/backjoon/Backjoon_11654.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjoon_11654 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String N = br.readLine();
+
+		System.out.println(N.charAt(0) - 0);
+	}
+
+}
