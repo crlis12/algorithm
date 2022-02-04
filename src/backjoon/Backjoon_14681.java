10a1b1fb3ffdc0ea3c5ef28eae7a7dc09ff29e43
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-02-04 22:52:19 +0900

    14681번

---
+++ src/backjoon/Backjoon_14681.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon_14681 {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int x,y;
+		
+		x= in.nextInt();
+		y= in.nextInt();
+		
+		
+		if(x > 0 && y > 0) {
+			System.out.println("1");
+		}else if(x < 0  && y > 0) {
+			System.out.println("2");
+		}else if(x < 0 && y < 0) {
+			System.out.println("3");
+		}else {
+			System.out.println("4");
+		}
+	}
+}
