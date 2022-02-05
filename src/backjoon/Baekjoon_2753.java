0e6067fa5d2080c3894d190f0114dfb2b7d9fdd7
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-02-05 23:29:50 +0900

    2753

---
+++ src/backjoon/Baekjoon_2753.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Baekjoon_2753 {
+ 
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		
+		if(a % 4 == 0 && a % 100 != 0 ) {
+			System.out.println("1");
+		}else if(a % 4 == 0 && a % 400 ==0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+}
